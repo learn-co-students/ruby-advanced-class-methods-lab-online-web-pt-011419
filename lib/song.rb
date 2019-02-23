9d89be7c4c7839d03abf142d16b3eba555b5b7f5
Kelsey Low <kklowlow@gmail.com>
2019-02-23 00:41:53 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,58 +1,58 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
   
-  def self.create  #class constructor
-    song = self.new  #initializes a song
-    self.all << song  #saves to Song.all
-    song  #returns the song instance that was initialized
-  end
-  
-  def self.new_by_name(title)  #class constructor
-    song = self.new  #initializes a song
-    song.name = title  #sets song name string as a property
-    song  #returns song instance with that name as a property
-  end
-  
-  def self.create_by_name(title)  #class constructor
-    song = self.create 
-    song.name = title 
+  def self.create
+    song = self.new
+    @@all << song
     song
   end
   
-  def self.find_by_name(title)  #class finder
-    result = self.all.detect{|song| song.name == title}
-    result
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
   end
   
-  def self.find_or_create_by_name(title)
-    result = self.find_by_name(title)
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+  
+  def self.find_by_name(name)
+    self.all.find{|person| person.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    result = self.find_by_name(name)
     if result
       result
     else
-      self.create_by_name(title)
+      self.create_by_name(name)
     end
   end
-
+  
   def self.alphabetical
-    sort = self.all.sort_by {|song| song.name}
-    sort
+    sorted = self.all.sort_by{|song| song.name}
+    sorted
   end
-
+  
   def self.new_from_filename(filename)
-    song_arr = filename.split(" - ")
-    song_arr[1] = song_arr[1].chomp(".mp3")
-    song = self.new
-    song.name = song_arr[1]
-    song.artist_name = song_arr[0]
+    array = filename.split(" - ") # split the filename into an array
+    array[1] = array[1].chomp(".mp3") # remove filetype
+    song = self.new # create an instance
+    # assign index 0 artist_name and index 1 name
+    song.artist_name = array[0]
+    song.name = array[1] 
     song
   end
   
@@ -63,7 +63,7 @@ class Song
     song.artist_name = result.artist_name
     song
   end
-
+  
   def self.destroy_all
     self.all.clear
   end
